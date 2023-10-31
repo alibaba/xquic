69646b64a5f2a95cac66e529d4efacfc20c7e6f9
zhaowu <zhaowu.zw@alibaba-inc.com>
2023-10-31 15:02:09 +0800

    [!] fix compile error on macos

---
+++ cmake/FindLibEvent.cmake
@@ -0,0 +1,48 @@
+# find the LibEvent library
+
+# find include dir
+find_path     (LIBEVENT_INCLUDE_DIR     NAMES event.h)
+
+# find dynamic library
+find_library  (LIBEVENT_LIBRARY         NAMES event)
+
+# find static library
+find_library  (LIBEVENT_LIBRARY_STATIC  NAMES libevent.a)
+
+
+# find version
+if(LIBEVENT_INCLUDE_DIR)
+    set(_version_regex "^#define[ ]+EVENT__VERSION[ ]+\"([^\"]+)\"")
+    file(STRINGS "${LIBEVENT_INCLUDE_DIR}/event2/event-config.h"
+        LIBEVENT_VERSION REGEX "${_version_regex}")
+    string(REGEX REPLACE "${_version_regex}" "\\1"
+        LIBEVENT_VERSION "${LIBEVENT_VERSION}")
+    unset(_version_regex)
+endif()
+
+
+
+include (FindPackageHandleStandardArgs)
+set (LIBEVENT_INCLUDE_DIRS ${LIBEVENT_INCLUDE_DIR})
+set (LIBEVENT_LIBRARIES
+    ${LIBEVENT_LIBRARY}
+    ${LIBEVENT_LIBRARY_STATIC}
+    ${LIBEVENT_SSL}
+    ${LIBEVENT_CORE}
+    ${LIBEVENT_EXTRA}
+    ${LIBEVENT_THREAD}
+    ${LIBEVENT_EXTRA})
+
+find_package_handle_standard_args(LibEvent
+    REQUIRED_VARS
+    LIBEVENT_INCLUDE_DIR
+    LIBEVENT_LIBRARY
+    LIBEVENT_LIBRARIES
+    LIBEVENT_LIBRARY_STATIC
+    VERSION_VAR LIBEVENT_VERSION
+)
+
+mark_as_advanced(
+    LIBEVENT_INCLUDE_DIRS
+    LIBEVENT_LIBRARIES
+)

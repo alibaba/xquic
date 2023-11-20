b6e4a87675e3bbf103640db521decb0113518021
Furong Yang <fryang@outlook.com>
2023-11-20 19:19:06 +0800

    Update case_test.sh
    loosing the success condition of testcase 'h3_ext_close_bytestream_during_transmission'

--- scripts/case_test.sh
+++ scripts/case_test.sh
@@ -3480,15 +3480,11 @@ clear_log
 echo -e "h3_ext_close_bytestream_during_transmission...\c"
 ./test_client -l d -T 2 -s 102400 -U 1 -Q 65535 -E -x 305 > stdlog
 cli_res1=`grep ">>>>>>>> pass:1" stdlog | wc -l`
-cli_res2=`grep "\[dgram\]|echo_check|same_content:yes|" stdlog`
-cli_res3=(`grep "\[h3-dgram\]|recv_dgram_bytes:" stdlog | egrep -o ':[0-9]+' | egrep -o '[0-9]+'`)
 cli_res4=(`grep "\[bytestream\]|bytes_sent:" stdlog | egrep -o ':[0-9]+' | egrep -o '[0-9]+'`)
 cli_res5=`grep "\[bytestream\]|same_content:.*|" stdlog | wc -l`
 cli_res6=`grep "xqc_h3_ext_bytestream_close|success" clog`
 errlog=`grep_err_log | grep -v "xqc_h3_stream_process_data|xqc_stream_recv"`
-if [ "$cli_res1" == "1" ] && [ -n "$cli_res2" ] \
-    && [ ${cli_res3[0]} -ge 102400 ] && [ ${cli_res3[1]} -ge 102400 ] \
-    && [ ${cli_res4[0]} -ge 102400 ] \
+if [ "$cli_res1" == "1" ] && [ ${cli_res4[0]} -ge 102400 ] \
     && [ "$cli_res5" == "1" ] && [ -n "$cli_res6" ] && [ -z "$errlog" ]; then
     echo ">>>>>>>> pass:1"
     case_print_result "h3_ext_close_bytestream_during_transmission" "pass"

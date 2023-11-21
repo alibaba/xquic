cff8287003d1133993564edf74cf05538040ce81
yuanbo_zhang <827750192@qq.com>
2023-11-21 14:09:49 +0800

    [~] fix xqc_pow2 by replace with xqc_pow2_upper

--- src/common/xqc_id_hash.h
+++ src/common/xqc_id_hash.h
@@ -30,26 +30,11 @@ typedef struct xqc_id_hash_table_s {
     xqc_allocator_t             allocator;
 } xqc_id_hash_table_t;
 
-
-/* make n to 2 pow  */
-static inline int
-xqc_pow2(unsigned int n)
-{
-    int clz , power = sizeof(n);
-    if (__builtin_popcount(n) <= 1) {
-        return n ;
-    }
-    clz     = __builtin_clz(n);
-    power   = (power << 3) - clz ;
-    return pow(2 , power);
-}
-
-
 static inline xqc_int_t
 xqc_id_hash_init(xqc_id_hash_table_t *hash_tab,  xqc_allocator_t allocator, size_t bucket_num)
 {
     hash_tab->allocator = allocator;
-    bucket_num = xqc_pow2(bucket_num);
+    bucket_num = xqc_pow2_upper(bucket_num);
     hash_tab->list = allocator.malloc(allocator.opaque, sizeof(xqc_id_hash_node_t *) * bucket_num);
     if (hash_tab->list == NULL) {
         return XQC_ERROR;

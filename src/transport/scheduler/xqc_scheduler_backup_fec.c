511937494920263c88d749c59764a3acdfe46c84
Lei Peng <plhitsz@outlook.com>
2026-03-17 17:16:00 +0800

    [=]: fix for updating fec_rep_path_id

--- src/transport/scheduler/xqc_scheduler_backup_fec.c
+++ src/transport/scheduler/xqc_scheduler_backup_fec.c
@@ -182,6 +182,17 @@ skip_path:
 #endif
 
     if (ret_path != NULL) {
+#ifdef XQC_ENABLE_FEC
+        if (conn->fec_ctl
+            && conn->fec_ctl->fec_mp_mode == XQC_FEC_MP_USE_STB
+            && (packet_out->po_frame_types & XQC_FRAME_BIT_REPAIR_SYMBOL))
+        {
+            conn->fec_ctl->fec_rep_path_id =
+                (ret_path->app_path_status == XQC_APP_PATH_STATUS_STANDBY)
+                ? ret_path->path_id
+                : XQC_MAX_UINT64_VALUE;
+        }
+#endif
         xqc_log(conn->log, XQC_LOG_DEBUG, "|best path:%ui|frame_type:%s|"
                 "pn:%ui|size:%ud|reinj:%d|path_class:%d|",
                 ret_path->path_id, 

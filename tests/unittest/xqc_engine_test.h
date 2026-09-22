df61220f2b1d337cbef839f34f316e3268a0108b
dreamwind1985 <tmliliang@qq.com>
2026-09-22 14:06:11 +0800

    [+]: Add NAT rebinding validation tests.

--- tests/unittest/xqc_engine_test.h
+++ tests/unittest/xqc_engine_test.h
@@ -7,5 +7,15 @@
 
 void xqc_test_engine_create();
 void xqc_test_engine_packet_process();
+void xqc_test_rebinding_candidate_budget_limit();
+void xqc_test_rebinding_no_padding_flag();
+void xqc_test_rebinding_min_padding_flag();
+void xqc_test_rebinding_response_state_transition();
+void xqc_test_rebinding_clear();
+void xqc_test_rebinding_ignores_unauthenticated_packet();
+void xqc_test_rebinding_path_challenge_requires_confirmed();
+void xqc_test_rebinding_timeout_retry_limit();
+void xqc_test_rebinding_response_mismatch_preserves_state();
+void xqc_test_rebinding_sender_error_clears_candidate();
 
 #endif
